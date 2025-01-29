9ad6af15ba67b5d2a91194208d8a4cf24d9cc913
Devin <devin.zy@outlook.com>
2025-01-30 01:07:27 +0800

    chore: Enable some CI
    --story=1

--- integration_test/evm_module/scripts/evm_tests.sh
+++ integration_test/evm_module/scripts/evm_tests.sh
@@ -4,7 +4,6 @@ set -e
 
 cd contracts
 npm ci
-#TODO:Devin uncomment the following line
-#npx hardhat test --network seilocal test/EVMCompatabilityTest.js
+npx hardhat test --network seilocal test/EVMCompatabilityTest.js
 npx hardhat test --network seilocal test/EVMPrecompileTest.js
 npx hardhat test --network seilocal test/AssociateTest.js
