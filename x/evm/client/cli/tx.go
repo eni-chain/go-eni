20651144a7867bde76513cb32717e8c8797e49b3
maxwell <maxwell0019@proton.me>
2025-03-13 11:39:23 +0800

    evm deploy command bug fix

--- x/evm/client/cli/tx.go
+++ x/evm/client/cli/tx.go
@@ -280,7 +280,7 @@ func CmdDeployContract() *cobra.Command {
 		Use:   "deploy [path to binary] [value] --from=<sender> --gas-fee-cap=<cap> --gas-limt=<limit> --evm-rpc=<url>",
 		Short: "Deploy an EVM contract for binary at specified path",
 		Long:  "",
-		Args:  cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
@@ -321,7 +321,7 @@ func CmdDeployContract() *cobra.Command {
 				}
 			}
 			val := utils.Big0
-			if n, err := cmd.Flags().GetInt64(FlagNonce); err == nil && n >= 0 {
+			if n, err := cmd.Flags().GetInt64(FlagValue); err == nil && n >= 0 {
 				val = big.NewInt(n)
 			}
 			txData, err := getTxData(cmd)
