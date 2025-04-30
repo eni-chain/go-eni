fd4734270a2764938c47103a8d46fc2643cd871f
Jonathan71240 <Jonathan71240@proton.me>
2025-04-30 11:26:33 +0800

    bugfix for auditPass log

--- syscontract/genesis/dpos/hub.sol
+++ syscontract/genesis/dpos/hub.sol
@@ -71,7 +71,7 @@ contract Hub is DelegateCallBase {
             a.pubKey
         );
 
-        llog(DEBUG, abi.encodePacked("auditPass, validator name:", a.name, ", operator:", a.operator, ", admin:", H(msg.sender),  ", pledge amount: ", S(a.amount)));
+        llog(DEBUG, abi.encodePacked("auditPass, validator name:", a.name, ", operator:", H(a.operator), ", admin:", H(msg.sender),  ", pledge amount: ", S(a.amount)));
         emit AuditPass(msg.sender, a.name, a.operator, a.node, a.pubKey, a.amount);
 
         delete _applicants[operator];
